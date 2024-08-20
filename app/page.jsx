ae200ae1dc821bfea5d4e03de98e996dd7acda7f
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-08-20 16:48:40 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-center">
+        <h1>Obo Tutor</h1>
+        <h1>Robotic Gen </h1>
+      </div>
+    </div>
+  );
+}
