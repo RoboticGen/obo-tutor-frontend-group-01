eeb4c208259641d09cd033328a095897c3456384
Pasindu Sankalpa <105929988+Pasindu599@users.noreply.github.com>
2024-08-21 09:28:14 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 import Image from "next/image";
-import { SunIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import Login from "@/components/Login";
-import { useGlobalContext } from "@/context/GlobalContextProvider";
 
 export default function Home() {
-  const { session } = useGlobalContext();
+  const session = false;
   const router = useRouter();
 
   if (session) {
